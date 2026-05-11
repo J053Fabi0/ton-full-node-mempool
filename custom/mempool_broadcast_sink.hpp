8be9723144e53f67ba74b23999b3d297b0c3f2ee
Jose Fabio <J053Fabi0@users.noreply.github.com>
2026-05-11 07:49:50 -0500

    fix

--- custom/mempool_broadcast_sink.hpp
+++ custom/mempool_broadcast_sink.hpp
@@ -3,6 +3,14 @@
 #include "td/actor/actor.h"
 #include "td/utils/buffer.h"
 #include "td/utils/port/detail/NativeFd.h"
+#include "td/utils/BufferedFd.h"
+#include "td/utils/port/SocketFd.h"
+#include "td/utils/logging.h"
+
+#include <arpa/inet.h>
+#include <sys/socket.h>
+#include <sys/un.h>
+#include <unistd.h>
 
 #include <memory>
 #include <string>
@@ -24,7 +32,77 @@ class MemPoolBroadcastSink : public td::actor::Actor {
  private:
   std::string socket_path_;
 
-  class SocketWriter;
+  class SocketWriter {
+   public:
+    explicit SocketWriter(const std::string &socket_path) : socket_path_(socket_path), connected_(false) {
+    }
+
+    void write_async(td::BufferSlice data) {
+      if (!connected_) {
+        if (!try_connect()) {
+          VLOG(FULL_NODE_WARNING) << "MemPoolBroadcastSink: socket not connected: " << socket_path_;
+          return;
+        }
+      }
+
+      uint32_t len = static_cast<uint32_t>(data.size());
+      uint32_t be_len = htonl(len);
+      char lenbuf[4];
+      memcpy(lenbuf, &be_len, 4);
+
+      buffered_fd_.output_buffer().append(td::BufferSlice(lenbuf, 4));
+      buffered_fd_.output_buffer().append(data);
+
+      auto res = buffered_fd_.flush_write();
+      if (res.is_error()) {
+        VLOG(FULL_NODE_WARNING) << "MemPoolBroadcastSink: write error: " << res.error();
+        connected_ = false;
+        buffered_fd_.close();
+      }
+    }
+
+   private:
+    bool try_connect() {
+      int fd = ::socket(AF_UNIX, SOCK_STREAM, 0);
+      if (fd < 0) {
+        VLOG(FULL_NODE_WARNING) << "MemPoolBroadcastSink: socket() failed";
+        return false;
+      }
+
+      struct sockaddr_un addr{};
+      addr.sun_family = AF_UNIX;
+      if (socket_path_.size() >= sizeof(addr.sun_path)) {
+        ::close(fd);
+        VLOG(FULL_NODE_WARNING) << "MemPoolBroadcastSink: socket path too long";
+        return false;
+      }
+      strncpy(addr.sun_path, socket_path_.c_str(), sizeof(addr.sun_path) - 1);
+
+      if (::connect(fd, reinterpret_cast<struct sockaddr *>(&addr), sizeof(addr)) != 0) {
+        ::close(fd);
+        VLOG(FULL_NODE_DEBUG) << "MemPoolBroadcastSink: connect() failed to " << socket_path_;
+        return false;
+      }
+
+      td::NativeFd native(fd);
+      auto r = td::SocketFd::from_native_fd(std::move(native));
+      if (r.is_error()) {
+        ::close(fd);
+        VLOG(FULL_NODE_WARNING) << "MemPoolBroadcastSink: from_native_fd failed: " << r.error();
+        return false;
+      }
+
+      buffered_fd_ = td::BufferedFd<td::SocketFd>(r.move_as_ok());
+      connected_ = true;
+      VLOG(FULL_NODE_DEBUG) << "MemPoolBroadcastSink: connected to " << socket_path_;
+      return true;
+    }
+
+    std::string socket_path_;
+    td::BufferedFd<td::SocketFd> buffered_fd_;
+    bool connected_;
+  };
+
   std::unique_ptr<SocketWriter> writer_;
 };
 
